d542cceea9a4cbab3f33d7b11dc4b391408da1a5
SokoMix <dimonsheu@mail.ru>
2024-04-11 11:28:31 +0300

    First sem changes

--- allocator/allocator_boundary_tags/src/allocator_boundary_tags.cpp
+++ allocator/allocator_boundary_tags/src/allocator_boundary_tags.cpp
@@ -1,4 +1,5 @@
 #include <not_implemented.h>
+#include <mutex>
 
 #include "../include/allocator_boundary_tags.h"
 
@@ -8,53 +9,97 @@ allocator_boundary_tags::~allocator_boundary_tags()
 }
 
 allocator_boundary_tags::allocator_boundary_tags(
-    allocator_boundary_tags const &other)
-{
-    throw not_implemented("allocator_boundary_tags::allocator_boundary_tags(allocator_boundary_tags const &)", "your code should be here...");
-}
-
-allocator_boundary_tags &allocator_boundary_tags::operator=(
-    allocator_boundary_tags const &other)
-{
-    throw not_implemented("allocator_boundary_tags &allocator_boundary_tags::operator=(allocator_boundary_tags const &)", "your code should be here...");
-}
-
-allocator_boundary_tags::allocator_boundary_tags(
-    allocator_boundary_tags &&other) noexcept
+        allocator_boundary_tags &&other) noexcept
 {
     throw not_implemented("allocator_boundary_tags::allocator_boundary_tags(allocator_boundary_tags &&) noexcept", "your code should be here...");
 }
 
 allocator_boundary_tags &allocator_boundary_tags::operator=(
-    allocator_boundary_tags &&other) noexcept
+        allocator_boundary_tags &&other) noexcept
 {
     throw not_implemented("allocator_boundary_tags &allocator_boundary_tags::operator=(allocator_boundary_tags &&) noexcept", "your code should be here...");
 }
 
 allocator_boundary_tags::allocator_boundary_tags(
-    size_t space_size,
-    allocator *parent_allocator,
-    logger *logger,
-    allocator_with_fit_mode::fit_mode allocate_fit_mode)
+        size_t space_size,
+        allocator *parent_allocator,
+        logger *logger,
+        allocator_with_fit_mode::fit_mode allocate_fit_mode)
 {
-    throw not_implemented("allocator_boundary_tags::allocator_boundary_tags(size_t, allocator *, logger *, allocator_with_fit_mode::fit_mode)", "your code should be here...");
+    if (space_size < get_occupied_block_metadata_size())
+    {
+        throw std::logic_error("Too low memory");
+    }
+
+    space_size += get_allocator_metadata_size();
+
+    try
+    {
+        _trusted_memory = parent_allocator == nullptr
+                          ? ::operator new(space_size)
+                          : parent_allocator->allocate(space_size, 1);
+    }
+    catch (std::bad_alloc const &ex)
+    {
+        // TODO: watch task!
+        throw;
+    }
+
+    allocator **parent_allocator_space = reinterpret_cast<allocator **>(_trusted_memory);
+    *parent_allocator_space = parent_allocator;
+
+    class logger **logger_space = reinterpret_cast<class logger **>(parent_allocator_space + 1);
+    *logger_space = logger;
+
+    size_t *space_size_space = reinterpret_cast<size_t *>(logger_space + 1);
+    *space_size_space = space_size;
+
+    std::mutex *sync_object_space = reinterpret_cast<std::mutex *>(space_size_space + 1);
+    //new (sync_object_space) std::mutex();
+    allocator::construct(sync_object_space);
+
+    allocator_with_fit_mode::fit_mode *fit_mode_space = reinterpret_cast<allocator_with_fit_mode::fit_mode *>(sync_object_space + 1);
+    *fit_mode_space = allocate_fit_mode;
+
+    void **first_block_address_space = reinterpret_cast<void **>(fit_mode_space + 1);
+    *first_block_address_space = nullptr;
+
+    // TODO: logs...
 }
 
 [[nodiscard]] void *allocator_boundary_tags::allocate(
-    size_t value_size,
-    size_t values_count)
+        size_t value_size,
+        size_t values_count)
 {
-    throw not_implemented("[[nodiscard]] void *allocator_boundary_tags::allocate(size_t, size_t)", "your code should be here...");
+    // TODO: std::unique_lock
+    std::lock_guard<std::mutex> x(get_sync_object());
+    /// block_size, previous_pointer, next_pointer, parent_pointer
+    void *child_allocator = ::operator new(sizeof(size_t *) + 2 * sizeof(allocator **) + sizeof(allocator **) + value_size * values_count);
+
+    size_t *size = reinterpret_cast<size_t *>(child_allocator);
+    *size = values_count * values_count;
+
+    allocator** parent_allocator = reinterpret_cast<allocator **>(size + 1);
+    *parent_allocator = this;
+//    allocator** next_pointer = reinterpret_cast<allocator **>(parent_allocator - 1);
+//    next_pointer = nullptr;
+    allocator** previous_pointer = reinterpret_cast<allocator **>(reinterpret_cast<size_t>(parent_allocator + 1));
+    for (void **p = reinterpret_cast<void **>(this->get_first_occupied_block_address()); reinterpret_cast<allocator **>(reinterpret_cast<size_t *>(p) + 1) + 1 !=
+            nullptr; ++p) {
+        allocator ** aloc = reinterpret_cast<allocator **>(p);
+        *aloc = reinterpret_cast<allocator *>(parent_allocator) + 1;
+        
+    }
 }
 
 void allocator_boundary_tags::deallocate(
-    void *at)
+        void *at)
 {
     throw not_implemented("void allocator_boundary_tags::deallocate(void *)", "your code should be here...");
 }
 
 inline void allocator_boundary_tags::set_fit_mode(
-    allocator_with_fit_mode::fit_mode mode)
+        allocator_with_fit_mode::fit_mode mode)
 {
     throw not_implemented("inline void allocator_boundary_tags::set_fit_mode(allocator_with_fit_mode::fit_mode)", "your code should be here...");
 }
@@ -71,10 +116,34 @@ std::vector<allocator_test_utils::block_info> allocator_boundary_tags::get_block
 
 inline logger *allocator_boundary_tags::get_logger() const
 {
-    throw not_implemented("inline logger *allocator_boundary_tags::get_logger() const", "your code should be here...");
+    return *reinterpret_cast<class logger **>(reinterpret_cast<allocator **>(_trusted_memory) + 1);
+
+    // return *reinterpret_cast<class logger **>(reinterpret_cast<unsigned char *>(_trusted_memory) + sizeof(allocator *));
 }
 
 inline std::string allocator_boundary_tags::get_typename() const noexcept
 {
-    throw not_implemented("inline std::string allocator_boundary_tags::get_typename() const noexcept", "your code should be here...");
+    return "allocator_boudary_tags";
+}
+
+size_t allocator_boundary_tags::get_occupied_block_metadata_size()
+{
+    return sizeof(size_t) + sizeof(void *) * 3;
+}
+
+size_t allocator_boundary_tags::get_allocator_metadata_size()
+{
+    return sizeof(allocator *) + sizeof(logger *) + sizeof(size_t) + sizeof(void *) + sizeof(std::mutex) + sizeof(allocator_with_fit_mode::fit_mode);
+}
+
+inline std::mutex& allocator_boundary_tags::get_sync_object()
+{
+    return *reinterpret_cast<std::mutex *>(reinterpret_cast<unsigned char *>(_trusted_memory) + sizeof(allocator *) + sizeof(logger *) + sizeof(size_t));
+}
+
+inline void *allocator_boundary_tags::get_first_occupied_block_address()
+{
+    return *reinterpret_cast<void **>(reinterpret_cast<unsigned char *>(_trusted_memory) + sizeof(allocator *) + sizeof(logger *) + sizeof(size_t) + sizeof(std::mutex) + sizeof(allocator_with_fit_mode::fit_mode));
+
+    // return *reinterpret_cast<void **>(reinterpret_cast<unsigned char *>(_trusted_memory) + get_allocator_metadata_size() - sizeof(void *));
 }
--- allocator/allocator_global_heap/src/allocator_global_heap.cpp
+++ allocator/allocator_global_heap/src/allocator_global_heap.cpp
@@ -5,45 +5,80 @@
 allocator_global_heap::allocator_global_heap(
     logger *logger)
 {
-    throw not_implemented("allocator_global_heap::allocator_global_heap(logger *)", "your code should be here...");
+    this->_logger = logger;
 }
 
 allocator_global_heap::~allocator_global_heap()
-{
-    throw not_implemented("allocator_global_heap::~allocator_global_heap()", "your code should be here...");
-}
+{}
 
 allocator_global_heap::allocator_global_heap(
     allocator_global_heap &&other) noexcept
 {
-    throw not_implemented("allocator_global_heap::allocator_global_heap(allocator_global_heap &&) noexcept", "your code should be here...");
+    if (other._logger != nullptr) {
+        this->_logger = other._logger;
+        other._logger = nullptr;
+    }
 }
 
 allocator_global_heap &allocator_global_heap::operator=(
     allocator_global_heap &&other) noexcept
 {
-    throw not_implemented("allocator_global_heap &allocator_global_heap::operator=(allocator_global_heap &&) noexcept", "your code should be here...");
+    if (this != &other)
+    {
+        this->_logger = other._logger;
+        other._logger = nullptr;
+    }
+
+    return *this;
 }
 
 [[nodiscard]] void *allocator_global_heap::allocate(
     size_t value_size,
     size_t values_count)
 {
-    throw not_implemented("[[nodiscard]] void *allocator_global_heap::allocate(size_t, size_t)", "your code should be here...");
+    try {
+        this->_logger->trace("Allocating memory");
+        void * pointer = ::operator new(sizeof (allocator *) + sizeof(size_t) + value_size * values_count);
+        size_t *size = reinterpret_cast<size_t *>(pointer);
+        *size = value_size * values_count;
+        allocator **p = reinterpret_cast<allocator **>(size + 1);
+        *p = this;
+        this->_logger->trace("Finished allocating memory");
+
+        return reinterpret_cast<void *>(p + 1);
+    } catch (std::bad_alloc const &e) {
+        error_with_guard(e.what());
+        throw;
+    }
 }
 
 void allocator_global_heap::deallocate(
     void *at)
 {
-    throw not_implemented("void allocator_global_heap::deallocate(void *)", "your code should be here...");
+    void *block = reinterpret_cast<void *>(reinterpret_cast<unsigned char *>(at) - sizeof(allocator *) - sizeof(size_t));
+    if (*reinterpret_cast<allocator **>(reinterpret_cast<unsigned char *>(block) + sizeof(size_t)) != this)
+    {
+        throw std::logic_error("I can't delete this");
+    }
+    size_t size = *reinterpret_cast<size_t *>(reinterpret_cast<unsigned char *>(at) - sizeof(size_t));
+    std::string data_out;
+    for (int i = 0; i < size; ++i) {
+        data_out += *(reinterpret_cast<unsigned char *>(at) + i);
+    }
+    // void **pointer = &at;
+    // pointer--;
+    // void **p = pointer;
+    // pointer--;
+    // size_t *size = reinterpret_cast<size_t *>(pointer);
+    ::operator delete(block);
 }
 
 inline logger *allocator_global_heap::get_logger() const
 {
-    throw not_implemented("inline logger *allocator_global_heap::get_logger() const", "your code should be here...");
+    return _logger;
 }
 
 inline std::string allocator_global_heap::get_typename() const noexcept
 {
-    throw not_implemented("inline std::string allocator_global_heap::get_typename() const noexcept", "your code should be here...");
+    return "allocator_global_heap";
 }
